bbaf7577f1629abd1642daefcec31ccbc0aeef13
EzequielVillalba112 <148280303+EzequielVillalba112@users.noreply.github.com>
2023-11-27 19:32:05 -0300

    final

--- src/components/Juego.jsx
+++ src/components/Juego.jsx
@@ -1,36 +1,100 @@
-import { useState } from 'react'
-import {Questions} from '../data/data'
-import { useEffect } from 'react';
+import { useState } from "react";
+import { Questions } from "../data/data";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {ImgAhorcado} from "../data/img"
 
 export default function Juego() {
- 
+  const navegacion = useNavigate();
+  const abecedario = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ".split("");
+  const colors = [{backgroundColor: '#1a1a1a'},{backgroundColor:'#108b54'}, {backgroundColor:'#d80000'}]
+  //guarda la posicion aleatoria
   const [azar, setAzar] = useState(0);
+  //gurada la respuesta
   const [palabra, setPalabra] = useState([]);
- 
-  useEffect(()=>{
-    //Consigue un numero al azar segun la cantidad de 
-    //preguntas que haya.
-    setAzar( Math.floor(Math.random() * Questions.length));
-  }, [])
+  //guarda las letras presionadas
+  const [misLetras, setMisLetras] = useState([]);
+  //guarda las letras correctas
+  const [correctas, setCorrectas] = useState([]);
+  //guarda las letras incorrectas
+  const [incorretas, setIncorretas] = useState([]);
 
-  useEffect(()=>{
-    //dividir un string en letras y representar cada 
+  const [img, setImg] = useState(1);
+  useEffect(() => {
+    //Consigue un numero al azar segun la cantidad de
+    //preguntas que haya.
+    setAzar(Math.floor(Math.random() * Questions.length));
+  }, []);
+
+  useEffect(() => {
+    //dividir un string en letras y representar cada
     //letra como un elemento individual utilizando el método split
     setPalabra(Questions[azar].palabro.split(""));
-  },[azar])
+  }, [azar]);
 
+  const selected = (e) =>{
+    const letra = e.target.innerHTML;
+    setMisLetras([...misLetras,letra]);
+
+    if(palabra.indexOf(letra) >= 0){
+      setCorrectas([...correctas, letra]);
+    }else{
+      setIncorretas([...incorretas, letra]);
+      setImg(img+1);
+      if(img == 5){
+        navegacion('/final');
+      }
+    }
+  }
+
+  useEffect(()=>{
+    let noEncontrado = 0;
+    palabra.map(palabra =>{
+      if(misLetras.find(e => e === palabra)=== undefined){
+        noEncontrado++;
+      }
+    })
+
+    if(noEncontrado === 0 && correctas.length > 0){
+      navegacion('/ganador');
+    }
+  },[correctas])
+
+  
   return (
-    <>
-      <div className="pregunta">
-        {Questions[azar].pregunta}
-      </div>
+    <div className="container-ahorcado">
+      <div className="pregunta">{Questions[azar].pregunta}</div>
       <div className="palabras">
-        {palabra.map((letra)=>{
-          <div className='letras'></div>
-        })}
+        {
+          palabra.map((letra, i) =>
+          //si no se encuentra en la cadena original, indexOf devuelve -1.
+          misLetras.indexOf(letra) === -1 ? (
+            <div className="letras" key={i}></div>
+          ) : (
+            <div className="letras" key={i}>
+              {letra.toUpperCase()}
+            </div>
+          )
+        )}
       </div>
-      <div className="botones"></div>
-      <div className="img"></div>
-    </>
-  )
+      <div className="botones">
+        {
+          abecedario.map((abeced) =>(
+            (correctas.find(e => e=== abeced))
+            ?
+            <button style={colors[1]} className="btn-abc" key={abeced}>{abeced}</button>
+            :
+            (incorretas.find(e => e=== abeced))
+            ?
+            <button style={colors[2]} className="btn-abc" key={abeced}>{abeced}</button>
+            :
+            <button style={colors[0]} onClick={selected} className="btn-abc" key={abeced}>{abeced}</button>
+            ))
+        }
+      </div>
+      <div className="img">
+        <img className="img-ahoracado" src={ImgAhorcado[img]}/>
+      </div>
+    </div>
+  );
 }
---
+++ src/data/img.js
@@ -0,0 +1,8 @@
+import img1 from '../assets/el_ahorcado1.png'
+import img2 from '../assets/el_ahorcado2.png'
+import img3 from '../assets/el_ahorcado3.png'
+import img4 from '../assets/el_ahorcado4.png'
+import img5 from '../assets/el_ahorcado5.png'
+import img6 from '../assets/el_ahorcado6.png'
+
+export const ImgAhorcado =[img1,img2,img3,img4,img5,img6]
